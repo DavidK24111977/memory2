3d218d5178eab85907209f060546da52077c53b5
DavidK <davidkips77@gmail.com>
2019-12-12 15:37:50 +0100

    img ok

--- app.js
+++ app.js
@@ -1,44 +1,69 @@
 
 function reveal() {
-    console.log(this.nom);
+    console.log(this.id);
+    return this.url;
 }
 var backImg="https://picsum.photos/id/77/300/200";
 
-
-var cards = [
+var cardsArray = [
     {
-        nom: "1",
+        id:1,
         reveal,
         url:"https://picsum.photos/id/1/300/200"
     },
     {
-        nom: "1",
+        id:1,
         reveal,
         url:"https://picsum.photos/id/1/300/200"
     },
     {
-        nom: "2",
+        id:2,
         reveal,
         url:"https://picsum.photos/id/2/300/200"
     },
     {
-        nom: "2",
+        id:2,
         reveal,
         url:"https://picsum.photos/id/2/300/200"
+    },
+    {
+        id:3,
+        reveal,
+        url:"https://picsum.photos/id/3/300/200"
+    },
+    {
+        id:3,
+        reveal,
+        url:"https://picsum.photos/id/3/300/200"
+    },
+    {
+        id:4,
+        reveal,
+        url:"https://picsum.photos/id/4/300/200"
+    },
+    {
+        id:4,
+        reveal,
+        url:"https://picsum.photos/id/4/300/200"
     }
 ];
-shuffle(cards);
+shuffle(cardsArray);
+
 var cardsDiv;
 var container=document.getElementById("container");
 var img;
-for (var card of cards) {
-    // card.reveal();
+for (const i in cardsArray) {
     cardsDiv=document.createElement("div");
     img=document.createElement("img");
     cardsDiv.className="cards";
     container.appendChild(cardsDiv);
     cardsDiv.appendChild(img);
     img.src=backImg;
+    img.onclick=function(){
+        // cardsArray[i].reveal();
+        this.src=cardsArray[i].reveal();
+    };
+
 }
 
 function shuffle(array) {
